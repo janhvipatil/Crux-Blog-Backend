8db6957ba3616c5a4a6912ab8a7a96b0800a287a
Janhvi <janhvipatil716@gmail.com>
2022-01-25 02:27:05 +0530

    initial commit

---
+++ schemas/post.js
@@ -0,0 +1,57 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'pollID',
+      title: 'Poll ID',
+      type: 'string',
+    },
+    {
+      name: 'subText',
+      title: 'Subtext',
+      type: 'string',
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+      subText: 'subText',
+    },
+  },
+}
